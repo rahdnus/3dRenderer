8075fff0757fcaf43afa10a9715d39137355a1cf
rahdnus <rahdnus119@gmail.com>
2022-11-01 16:24:29 +0530

    zBuffering

--- Base/main.cpp
+++ Base/main.cpp
@@ -1,209 +1,123 @@
+#include <vector>
+#include <cmath>
+#include <cstdlib>
+#include <limits>
+#include <array>
 #include "tgaimage.h"
 #include "model.h"
-#include <cmath>
+#include "geometry.h"
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
-const TGAColor red = TGAColor(255, 0, 0, 255);
-const TGAColor green = TGAColor(0, 255, 0, 255);
+const TGAColor red   = TGAColor(255, 0,   0,   255);
+Model *model = NULL;
+const int width  = 800;
+const int height = 800;
+Vec3f light_dir(0,0,1);
 
-const int width=800;
-const int height=800;
-Model *model;
 
-Vec3f light_dir(0,0,-1);
-
-void draw_line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color)
+inline Vec3f cross(const Vec3f& v1, const Vec3f& v2)
 {
-	bool steep = false;
-	if (std::abs(x1 - x0) < std::abs(y1 - y0))
-	{
-		std::swap(x0, y0);
-		std::swap(x1, y1);
-		steep = true;
-	}
-	if (x1 < x0)
-	{
-		std::swap(x0, x1);
-		std::swap(y0, y1);
-	}
-
-	int dy = y1 - y0;
-	int dx = x1 - x0;
-	float derror = std::abs(dy) * 2;
-	float error = 0;
-	int y = y0;
-	int yincr = (y1 > y0 ? 1 : -1);
-	if (steep)
-	{
-		for (float x = x0; x < x1; x++)
-		{
-			image.set(y, x, color);
-			error += derror;
-			if (error > 0.5)
-			{
-				y += yincr;
-				error -= dx * 2;
-			}
-		}
-	}
-
-	else
-	{
-		for (float x = x0; x < x1; x++)
-		{
-			image.set(x, y, color);
-			error += derror;
-			if (error > 0.5)
-			{
-				y += yincr;
-				error -= dx * 2;
-			}
-		}
-	}
+    return Vec3f{v1.y * v2.z - v1.z * v2.y, v1.z * v2.x - v1.x * v2.z, v1.x * v2.y - v1.y * v2.x};
 }
 
-void draw_wireframe(std::string address,TGAImage &image)
-{
-	model=new Model(address.c_str());
-	for(int i=0;i<model->nfaces();i++)
-	{
-		std::vector<int> faces=model->face(i);
-		for(int j=0;j<3;j++)
-		{
-			Vec3f v0=model->vert(faces[j]);
-			Vec3f v1=model->vert(faces[(j+1)%3]);
-			
- 		int x0 = (v0.x+1.)*width/2.; 
-        int y0 = (v0.y+1.)*height/2.; 
-        int x1 = (v1.x+1.)*width/2.; 
-        int y1 = (v1.y+1.)*height/2.; 
-		draw_line(x0,y0,x1,y1,image,white);
-		}
-	}
+void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
+    bool steep = false;
+    if (std::abs(x0-x1)<std::abs(y0-y1)) {
+        std::swap(x0, y0);
+        std::swap(x1, y1);
+        steep = true;
+    }
+    if (x0>x1) {
+        std::swap(x0, x1);
+        std::swap(y0, y1);
+    }
+
+    for (int x=x0; x<=x1; x++) {
+        float t = (x-x0)/(float)(x1-x0);
+        int y = y0*(1.-t) + y1*t;
+        if (steep) {
+            image.set(y, x, color);
+        } else {
+            image.set(x, y, color);
+        }
+    }
 }
 
-Vec3f barycentric(Vec2i *pts, Vec2i P) { 
-    Vec3f u = Vec3f(pts[2].x-pts[0].x, pts[1].x-pts[0].x, pts[0].x-P.x)^Vec3f(pts[2].y-pts[0].y, pts[1].y-pts[0].y, pts[0].y-P.y);
-    /* `pts` and `P` has integer value as coordinates
-       so `abs(u[2])` < 1 means `u[2]` is 0, that means
-       triangle is degenerate, in this case return something with negative coordinates */
-    if (std::abs(u.z)<1) return Vec3f(-1,1,1);
-    return Vec3f(1.f-(u.x+u.y)/u.z, u.y/u.z, u.x/u.z); 
-} 
- /*
- public static bool PointInTriangle(Point A, Point B, Point C, Point P)
-	{
-			double s1 = C.y - A.y;
-			double s2 = C.x - A.x;
-			double s3 = B.y - A.y;
-			double s4 = P.y - A.y;
+Vec3f barycentric(Vec3f A, Vec3f B, Vec3f C, Vec3f P) {
+    Vec3f s[2];
+    for (int i=2; i--; ) {
+        s[i][0] = C[i]-A[i];
+        s[i][1] = B[i]-A[i];
+        s[i][2] = A[i]-P[i];
+    }
+    Vec3f u = cross(s[0], s[1]);
+    if (std::abs(u[2])>1e-2) // dont forget that u[2] is integer. If it is zero then triangle ABC is degenerate
+        return Vec3f(1.f-(u.x+u.y)/u.z, u.y/u.z, u.x/u.z);
+    return Vec3f(-1,1,1); // in this case generate negative coordinates, it will be thrown away by the rasterizator
+}
 
-			double w1 = (A.x * s1 + s4 * s2 - P.x * s1) / (s3 * s2 - (B.x-A.x) * s1);
-			double w2 = (s4- w1 * s3) / s1;
-			return w1 >= 0 && w2 >= 0 && (w1 + w2) <= 1;
-	}
+void triangle(Vec3f *pts, float *zbuffer, TGAImage &image, TGAColor color) {
+    Vec2f bboxmin( std::numeric_limits<float>::max(),  std::numeric_limits<float>::max());
+    Vec2f bboxmax(-std::numeric_limits<float>::max(), -std::numeric_limits<float>::max());
+    Vec2f clamp(image.get_width()-1, image.get_height()-1);
+    for (int i=0; i<3; i++) {
+        for (int j=0; j<2; j++) {
+            bboxmin[j] = std::max(0.f,      std::min(bboxmin[j], pts[i][j]));
+            bboxmax[j] = std::min(clamp[j], std::max(bboxmax[j], pts[i][j]));
+        }
+    }
+
+
+
+
 	
- */
-void triangle(Vec2i *pts, TGAImage &image, TGAColor color) { 
-    Vec2i bboxmin(image.get_width()-1,  image.get_height()-1); 
-    Vec2i bboxmax(0, 0); 
-    Vec2i clamp(image.get_width()-1, image.get_height()-1); 
-    for (int i=0; i<3; i++) { 
-        bboxmin.x = std::max(0, std::min(bboxmin.x, pts[i].x));
-	bboxmin.y = std::max(0, std::min(bboxmin.y, pts[i].y));
+    Vec3f P;
+    for (P.x=bboxmin.x; P.x<=bboxmax.x; P.x++) {
+        for (P.y=bboxmin.y; P.y<=bboxmax.y; P.y++) {
+            Vec3f bc_screen  = barycentric(pts[0], pts[1], pts[2], P);
+            if (bc_screen.x<0 || bc_screen.y<0 || bc_screen.z<0) continue;
+            P.z = 0;
+            for (int i=0; i<3; i++) P.z += pts[i][2]*bc_screen[i];
+            if (zbuffer[int(P.x+P.y*width)]<P.z) {
+                zbuffer[int(P.x+P.y*width)] = P.z;
 
-	bboxmax.x = std::min(clamp.x, std::max(bboxmax.x, pts[i].x));
-	bboxmax.y = std::min(clamp.y, std::max(bboxmax.y, pts[i].y));
-    } 
-    Vec2i P; 
-    for (P.x=bboxmin.x; P.x<=bboxmax.x; P.x++) { 
-        for (P.y=bboxmin.y; P.y<=bboxmax.y; P.y++) { 
-            Vec3f bc_screen  = barycentric(pts, P); 
-            if (bc_screen.x<0 || bc_screen.y<0 || bc_screen.z<0) continue; 
-            image.set(P.x, P.y, color); 
-        } 
-    } 
-} 
-
-void draw_flat(std::string address,TGAImage &image)
-{
-	model=new Model(address.c_str());
-	for(int i=0;i<model->nfaces();i++)
-	{
-		std::vector<int> face=model->face(i);
-		Vec2i screencords[3];
-		Vec3f objectcords[3];
-
-		for(int j=0;j<3;j++)
-		{
-			Vec3f coords=model->vert(face[j]);
-			screencords[j]=Vec2i((coords.x+1)*width/2,(coords.y+1)*height/2);
-			objectcords[j]=coords;
-		}
-
-		Vec3f n=(objectcords[2]-objectcords[0])^(objectcords[1]-objectcords[0]);
-		n.normalize();
-		float intensity=n*light_dir;
-		if(intensity>0)
-			triangle(screencords,image,TGAColor(intensity*255, intensity*255, intensity*255, 255));
-	}
+                image.set(P.x, P.y, color);
+            }
+        }
+    }
 }
 
-
-#pragma region oldmethod
-void draw_triangle(Vec2i v0,Vec2i v1,Vec2i v2,TGAImage &image,TGAColor color)
-{
-	if(v0.y>v1.y){std::swap(v0,v1);}
-	if(v0.y>v2.y){std::swap(v2,v0);}
-	if(v1.y>v2.y){std::swap(v1,v2);}
-
-	int totalheight=v2.y-v0.y;
-	int segmentheight=v1.y-v0.y+1;
-	for(int y=v0.y;y<=v1.y;y++)
-	{
-		float alpha=(float)(y-v0.y)/totalheight;
-		float beta=(float)(y-v0.y)/segmentheight;
-		Vec2i A=v0+(v2-v0)*alpha;
-		Vec2i B=v0+(v1-v0)*beta;
-		if(A.x>B.x){std::swap(A,B);}
-		for(int x=A.x;x<=B.x;x++)
-		{
-			image.set(x,y,color);
-		}
-	}
-	int segmentBheight=v2.y-v1.y+1;
-
-	for(int y=v1.y;y<=v2.y;y++)
-	{
-		float alpha=(float)(y-v0.y)/totalheight;
-		float beta=(float)(y-v1.y)/segmentBheight;
-		Vec2i A=v0+(v2-v0)*alpha;
-		Vec2i B=v1+(v2-v1)*beta;
-		if(A.x>B.x){std::swap(A,B);}
-		for(int x=A.x;x<=B.x;x++)
-		{
-			image.set(x,y,color);
-		}
-	}
+Vec3f world2screen(Vec3f v) {
+    return Vec3f(int((v.x+1.)*width/2.+.5), int((v.y+1.)*height/2.+.5), v.z);
 }
-#pragma endregion oldmethod
 
-int main(int argc, char **argv)
-{
-	TGAImage image(width, height, TGAImage::RGB);
-	// draw_wireframe("obj/head.obj",image);
-    // Vec2i t0[3] = {Vec2i(10, 70),   Vec2i(50, 160),  Vec2i(70, 80)};
-    // Vec2i t1[3] = {Vec2i(180, 50),  Vec2i(150, 1),   Vec2i(70, 180)};
-    // Vec2i t2[3] = {Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180)};
-	// Vec2i pts[3] = {Vec2i(10,10), Vec2i(100, 30), Vec2i(190, 160)}; 
-	// draw_triangle(t0[0], t0[1], t0[2], image, red);
-	// draw_triangle(t1[0], t1[1], t1[2], image, white);
-	// draw_triangle(t2[0], t2[1], t2[2], image, green);
-	// triangle(pts,image,red);
-	draw_flat("obj/head.obj",image);
+int main(int argc, char** argv) {
+        model = new Model("obj/head.obj");
 
-	// i want to have the origin at the left bottom corner of the image
-	image.flip_vertically();
-	image.write_tga_file("output.tga");
-	return 0;
+    float *zbuffer = new float[width*height];
+    for (int i=width*height; i--; zbuffer[i] = -std::numeric_limits<float>::max());
+
+    TGAImage image(width, height, TGAImage::RGB);
+	
+    for (int i=0; i<model->nfaces(); i++) {
+        std::vector<int> face = model->face(i);
+        Vec3f pts[3];
+    	Vec3f world_coords[3]; 
+
+        for (int j=0; j<3; j++)
+		{
+		 pts[j] = world2screen(model->vert(face[j]));
+       	 world_coords[j]  = model->vert(face[j]); 
+		}
+   	 	Vec3f n = cross(world_coords[2]-world_coords[0],world_coords[1]-world_coords[0]); 
+	 	n.normalize();
+    	float intensity = n*light_dir; 
+        triangle(pts, zbuffer, image, TGAColor(255*intensity,255*intensity,255*intensity,255));
+    }
+
+    image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
+    image.write_tga_file("output.tga");
+    delete model;
+    return 0;
 }
+
